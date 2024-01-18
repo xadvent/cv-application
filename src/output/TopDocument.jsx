3b442afa19159e2a91b81ea9ca49bcca668b96ea
xadvent <piercestrutt@gmail.com>
2024-01-17 21:41:06 -0700

    fix(topdocument.jsx): img's do not display when there's no relevant info
    super small change, but adds flexibility to the document's header section.

--- src/output/TopDocument.jsx
+++ src/output/TopDocument.jsx
@@ -2,26 +2,38 @@ import './TopDocument.css';
 import propTypes from 'prop-types';
 
 export default function TopDocument({ name, email, phone = null, address }) {
-    if(phone) {
-        phone = '+1 (' + phone.slice(0, 3) + ') ' + phone.slice(3,6) + '-' + phone.slice(6)
+    if (phone) {
+        phone = '+1 (' + phone.slice(0, 3) + ') ' + phone.slice(3, 6) + '-' + phone.slice(6)
     }
 
     return (
         <div id="header">
             <h1>{name}</h1>
             <div id='contact-info' >
-                <div className="header-info">
-                    <img src="#" />
-                    <p>{email}</p>
-                </div>
-                <div className="header-info">
-                    <img src="#" />
-                    <p>{phone}</p>
-                </div>
-                <div className="header-info">
-                    <img src="#" />
-                    <p>{address}</p>
-                </div>
+                {
+                    email != '' ?
+                        <div className="header-info">
+                            <img src="#" />
+                            <p>{email}</p>
+                        </div>
+                        : null
+                }
+                {
+                    phone != '' ?
+                        <div className="header-info">
+                            <img src="#" />
+                            <p>{phone}</p>
+                        </div>
+                        : null
+                }
+                {
+                    address != '' ?
+                        <div className="header-info">
+                            <img src="#" />
+                            <p>{address}</p>
+                        </div>
+                        : null
+                }
             </div>
         </div>
     );
