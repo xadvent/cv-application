d3ab025adec821af395bd3b90737eb7bd40e01c1
xadvent <piercestrutt@gmail.com>
2024-01-20 20:13:05 -0700

    refactor(card creation): card.jsx fixed + small style changes
    Cards do not toggle and directly manipulate dom elements, fixing some small bugs.

--- src/Boxes/General Functions/Card.jsx
+++ src/Boxes/General Functions/Card.jsx
@@ -2,26 +2,39 @@ import { useState } from "react";
 import propTypes from 'prop-types';
 import './Card.css';
 
-export default function Card({ children, name, expandable, title}) {
+export default function Card({ children, name, expandable, title }) {
     const [expanded, setExpanded] = useState(false);
-    const show = function (e) {
-        let container = e.target.parentElement.parentElement;
-        container = container.querySelector('.expand-container');
 
-        setExpanded(!expanded);
-        container.classList.toggle('hidden');
+    const toggleExpand = () => {
+        setExpanded(!expanded)
     }
 
     if (expandable) {
         return (
             <div className={'card' + ' ' + name}>
-                <div className="title-container">
+                <div className="title-container" onClick={toggleExpand}>
                     <h2 className="card-title">{title}</h2>
-                    <div className="expand-icon" onClick={show}>{expanded ? 'X' : 'V'}</div>
+                    <div className="open-close">
+                        {
+                            expanded ?
+                                <>
+                                    <div className="left-card-icon close"></div>
+                                    <div className="right-card-icon close"></div>
+                                </>
+                                :
+                                <>
+                                    <div className="left-card-icon show"></div>
+                                    <div className="right-card-icon show"></div>
+                                </>
+                        }
+                    </div>
                 </div>
-                <div className="expand-container hidden">
-                    {children}
-                </div>
+                {
+                    expanded &&
+                    <div className="expand-container">
+                        {children}
+                    </div>
+                }
             </div>
         );
     }
