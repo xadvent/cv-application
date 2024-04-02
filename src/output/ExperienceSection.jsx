ad735c80211f50e140264f2879c93c4163536a67
xadvent <piercestrutt@gmail.com>
2024-04-01 18:30:23 -0600

    (style) add styles for the experience section
    Note: I know I repeated myself regarding the CSS. I'm going to refactor it.

--- src/output/ExperienceSection.jsx
+++ src/output/ExperienceSection.jsx
@@ -1,32 +1,45 @@
-import '../CSS/ExperienceSection.css';
-import propTypes from 'prop-types';
+import "../CSS/ExperienceSection.css";
+import propTypes from "prop-types";
 
 export default function ExperienceSection({ experience }) {
-    const experienceItems = experience.map((item, index) => formatExperience(item, index));
+  const experienceItems = experience.map((item, index) =>
+    formatExperience(item, index),
+  );
 
-    function formatExperience(item, index) {
-        return (
-            <div key={index} className="experience-item">
-                <div className="left-experience">
-                    <div className="date">{item.startDate + ' - ' + item.endDate}</div>
-                    <div className="location">{item.location ? item.location : ''}</div>
-                </div>
-                <div className="right-experience">
-                    <div className="work-company">{item.name}</div>
-                    <div className="work-title">{item.title}</div>
-                    <div className="work-description">{item.description}</div>
-                </div>
-            </div>
-        )
-    }
-    return experienceItems.length > 0 ? (
-        <div className="section">
-            <div className="banner">Experience</div>
-            {experienceItems}
+  function formatExperience(item, index) {
+    return (
+      <div key={index} className="experience-item">
+        <div className="left-experience">
+          <div className="date">{item.startDate + " - " + item.endDate}</div>
+          <div className="location">{item.location ? item.location : ""}</div>
         </div>
-    ) : null;
+        <div className="right-experience">
+          <div className="work-title" style={{ display: "flex" }}>
+            <div className="job-title" style={{ fontWeight: "bold" }}>
+              {item.title}
+            </div>
+            <div className="company" style={{ fontStyle: "italic" }}>
+              {" - " + item.name}
+            </div>
+          </div>
+          <div
+            className="work-description"
+            style={{ maxWidth: "95%", minHeight: "min-content", margin: "0px" }}
+          >
+            {item.description}
+          </div>
+        </div>
+      </div>
+    );
+  }
+  return experienceItems.length > 0 ? (
+    <div className="section">
+      <div className="banner">Experience</div>
+      {experienceItems}
+    </div>
+  ) : null;
 }
 
 ExperienceSection.propTypes = {
-    experience: propTypes.array.isRequired
-}
+  experience: propTypes.array.isRequired,
+};
