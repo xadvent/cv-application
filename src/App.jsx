609cef1f0e374b7e46603ae0882282b4ec196354
xadvent <piercestrutt@gmail.com>
2024-04-10 15:43:22 -0600

    feat: add pdf download functionality

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,25 @@ import EducationForm from './components/input/EducationForm';
 import EducationDocument from './components/output/EducationDocument';
 import Form from './components/input/Form'
 import TopDocument from './components/output/TopDocument';
+import { usePDF } from "react-to-pdf";
 import { useState } from 'react';
 import ExperienceForm from './components/input/ExperienceForm';
 import ExperienceSection from './components/output/ExperienceSection';
 
 export default function App() {
-  const [fullName, setFullName] = useState('');
-  const [email, setEmail] = useState('');
-  const [phone, setPhone] = useState('');
   const [address, setAddress] = useState('');
   const [education, setEducation] = useState([]);
+  const [email, setEmail] = useState('');
+  const [fullName, setFullName] = useState('');
+  const [phone, setPhone] = useState('');
   const [workExperience, setWorkExperience] = useState([]);
 
   const buttonStyle = {
     fontSize: 14 + 'px',
     fontWeight: 500,
   };
+
+  const { toPDF, targetRef } = usePDF({'filename': 'resume.pdf', 'method': 'open'})
 
   return (
     <>
@@ -31,9 +34,7 @@ export default function App() {
             document.querySelectorAll('input').forEach(target => target.value = '');
           }}>Clear Resume</button>
 
-          <button style={buttonStyle} id='download' onClick={() => {
-            console.log(workExperience)
-          }}>Download Resume</button>
+            <button style={buttonStyle} id='download' onClick={() => {toPDF()}}>Download Resume</button>
 
         </Card>
 
@@ -52,7 +53,7 @@ export default function App() {
       </div>
 
       <div id="right-side">
-        <div id="doc">
+        <div id="doc" ref={targetRef}>
           <TopDocument
             name={fullName}
             email={email}
